1e107ec2e2f950394196ee8ed49a88a36ced65a8
jhuang429 <jhuang9240@gmail.com>
2020-02-02 11:00:34 -0500

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  resources :seas
+
+  # get 'articles/:id', to: "articles#show"
+  # get 'articles/:id/new', to: 'articles#new'
+  # post 'articles/:id', to: 'articles#create'
+  # get 'articles/:id/edit', to: 'article#edit'
+  # patch 'articles/:id', to: 'article#update'
 end
